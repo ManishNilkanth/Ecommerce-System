9141181e70f8d9d651b7f4f61b367bbfce885531
Manish Nilkanth <109939260+ManishNilkanth@users.noreply.github.com>
2024-10-19 01:02:50 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/e_commerce/e_commerce_system/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/e_commerce/e_commerce_system/ExceptionHandler/GlobalExceptionHandler.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // Handle ProductNotFoundException
+ 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handleProductNotFound(ProductNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    // Handle UserNotFoundException
+  
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    // Handle InsufficientStockException
+  
     @ExceptionHandler(InsufficientStockException.class)
     public ResponseEntity<String> handleInsufficientStock(InsufficientStockException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    // Optionally, you can add a generic handler for any unhandled exceptions
+    
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex) {
         return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
